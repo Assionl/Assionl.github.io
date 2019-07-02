1ad5ab8473278150ad75a864d254de143b20e88f
Assionl <51063875+Assionl@users.noreply.github.com>
2019-07-02 11:39:55 +0800

    Add files via upload

---
+++ 0702贪吃蛇/js/my-tools.js
@@ -0,0 +1,216 @@
+//生成随机数
+function randomNumber(a = 1, b = 10) {
+	var min = Math.min(a, b);
+	var max = Math.max(a, b);
+	//console.log(min, max);
+	if (isNaN(max) || isNaN(min)) {
+		return; //类似break,结束本次函数的执行
+	}
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+/* 
+	定义个函数bind
+	参数：
+		obj：绑定事件的对象
+		eventStr 事件的字符串(不要on)
+		callback 回调函数
+ */
+function bind(obj, eventStr, callback) {
+	if (obj.addEventListener) {
+		//大部分浏览器兼容的模式
+		obj.addEventListener(eventStr, callback, false);
+	} else {
+		/* 
+			this是谁由调用方式决定
+			callback.call(obj);
+		 */
+		//IE8以下
+		obj.attachEvent("on" + eventStr, function() {
+			//匿名函数中调用回调函数
+			callback.call(obj);
+		});
+	}
+}
+
+//浏览器高度
+function getClientHeight() {
+	return window.innerHeight || document.documentElement.clientHeight;
+}
+//上滚动距离
+function getScroolTop() {
+	return document.documentElement.scrollTop;
+}
+
+//打乱顺序(洗牌)
+//参数：打乱数组的名字
+function randomArr(array) {
+	array.sort(function(a, b) {
+		return randomNumber(1, 10) - 5;
+	})
+}
+
+function randomColor(obj) {
+	var r = randomNumber(0, 255);
+	var g = randomNumber(0, 255);
+	var b = randomNumber(0, 255);
+	var h = randomNumber(10, 200);
+	obj.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+}
+
+//定义一个函数,用来向一个元素中添加指定的class属性值
+/* 
+	参数：
+		obj 要添加class属性的元素
+		cn 要添加的class值
+ */
+function addClass(obj, cn) {
+	//检查obj中是否含有cn
+	if (!hasClass(obj, cn)) {
+		obj.className += " " + cn;
+	}
+}
+
+/* 
+ *	判断一个元素中是否含有指定的class属性值
+ *	如果有该class,则返回true,没有则返回false 
+ */
+function hasClass(obj, cn) {
+	//var reg = /\bb2\b/;
+	var reg = new RegExp("\\b" + cn + "\\b");
+	return reg.test(obj.className);
+}
+
+//删除一个元素中指定的class属性
+function removeClass(obj, cn) {
+	var reg = new RegExp("\\b" + cn + "\\b");
+	//删除class
+	obj.className = obj.className.replace(reg, "")
+}
+
+/* 
+	切换一个类
+	如果元素中具有该类，则删除
+	如果元素中没有该类，则添加
+ */
+function toggleClass(obj, cn) {
+	if (hasClass(obj, cn)) {
+		removeClass(obj, cn);
+	} else {
+		addClass(obj, cn);
+	}
+}
+
+//获取指定元素距离页面最顶端的距离
+function getOffectTop(obj) {
+	// obj.offsetTop + obj.offsetParent.clientTop;
+	var top = 0;
+	while (true) {
+		top += obj.offsetTop + obj.offsetParent.clientTop;
+		//更新偏移父元素
+		obj = obj.offsetParent;
+		//body 的offsetParent为null
+		if (obj.offsetParent == null) {
+			break;
+		}
+	}
+	return top;
+};
+
+// 以对象形式封装预加载................................
+function loading(Obj) {
+	//记录下载的个数
+	var n = 0;
+	//缓存所有图片
+	Obj.imgArray.forEach(function(value, index) {
+		//创建吐图片对象
+		var imgObj = new Image();
+		//指定地址开始缓存
+		imgObj.src = value;
+		//监听缓存过程
+		imgObj.onload = function() {
+			n++;
+			//下载进度
+			//progress.value = n / imgArray.length;
+			var floatValue = n / Obj.imgArray.length;
+			//函数回调的安全判断
+			Obj.loadingCallback && Obj.loadingCallback(floatValue);
+
+			//判断所有图片都下载完成
+			if (n === Obj.imgArray.length) {
+				//mask.style.display = "none";
+				// successCallback();
+				setTimeout(Obj.successCallback, 100)
+			}
+		};
+	});
+};
+
+// 第三个参数单位是天， 几天后过期，给一个负数，就是删除它
+function setCookie(cname, cvalue, exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+	var expires = "expires=" + d.toGMTString();
+	document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i].trim();
+		if (c.indexOf(name) == 0) return c.substring(name.length);
+	}
+	return "";
+}
+
+/* 
+	提取专门用来拖拽的函数
+	参数：obj 用来拖拽的元素
+*/
+function drag(obj) {
+	obj.onmousedown = function(event) {
+		if (obj.setCapture) {
+			obj.setCapture();
+		}
+		var ol = event.clientX - obj.offsetLeft;
+		var op = event.clientY - obj.offsetTop;
+		document.onmousemove = function(event) {
+			event = event || window.event;
+			var left = event.clientX - ol;
+			var top = event.clientY - op;
+			obj.style.left = left + "px";
+			obj.style.top = top + "px";
+		};
+		document.onmouseup = function() { //无论在哪都在document上
+			document.onmousemove = null;
+			document.onmouseup = null;
+			if (obj.releaseCapture) {
+				obj.releaseCapture();
+			}
+
+		};
+		return false;
+	};
+};
+
+//监听浏览器大小设置字体【移动端】
+(function(doc, win) {
+	var html = doc.getElementsByTagName("html")[0],
+		// orientationchange->手机屏幕转屏事件
+		// resize->页面大小改变事件(一边pc端有用)
+		reEvt = "orientationchange" in win ? "orientationchange" : "resize",
+		reFontSize = function() {
+			var clientW = doc.documentElement.clientWidth || doc.body.clientWidth;
+			//alert(clientW);
+			if (!clientW) {
+				return;
+			}
+			//假设在iphone6的375屏幕给html设置的字体大小是100px，通过比例计算其他屏幕的字体尺寸
+			//f/320=100/375: 
+			html.style.fontSize = 100 * (clientW / 375) + "px";
+		}
+	win.addEventListener(reEvt, reFontSize);
+	// DOMContentLoaded->dom加载完就执行,onload要dom/css/js都加载完才执行
+	doc.addEventListener("DOMContentLoaded", reFontSize);
+})(document, window);
---
+++ 0702贪吃蛇/js/贪吃蛇.js
@@ -0,0 +1,199 @@
+var score = document.getElementById("score");
+var areaMap = document.getElementById("area");
+//定义食物的位置
+var foodX = 0;
+var foodY = 0;
+//为了灵活创建地图，将地图的行数和列数存储到变量中
+var rowNumber = 24;
+var colNumber = 16;
+var cellWidth = 20;
+areaMap.style.width = cellWidth * colNumber + "px";
+areaMap.style.height = cellWidth * rowNumber + "px";
+//二维数组 用来存储地图上所有位置的表格	为了获取div所在的位置 [[2,4],[3,6]]
+var divSnake = [];
+var rowArr;
+for (var i = 0; i < rowNumber; i++) {
+	//创建行所在的div
+	var rowDiv = document.createElement("div");
+	rowDiv.style.height = cellWidth + "px";
+	areaMap.appendChild(rowDiv);
+	//创建一个数组，用来存储每一行的所有表格
+	rowArr = [];
+	for (var j = 0; j < colNumber; j++) {
+		//创建每行中的小方格div
+		var cell = document.createElement("div");
+		cell.className = "cell";
+		cell.style.width = cellWidth + "px";
+		cell.style.height = cellWidth + "px";
+		rowDiv.appendChild(cell);
+		//将该行的表格添加到该行数组中
+		rowArr.push(cell);
+	}
+	//最后将行数组里的div全部添加到二维数组中
+	divSnake.push(rowArr);
+	// console.log(divSnake);
+}
+
+//创建数组，存储蛇身所占的div 默认第一行前三个是蛇身体
+var snakeBodys = [];
+for (var i = 0; i < 3; i++) {
+	snakeBodys.push(divSnake[0][i]);
+	// snakeBodys[i] = divSnake[0][i];
+	// 设置蛇身的颜色
+	// divSnake[0][i].addClass = "moveSnake";
+	addClass(divSnake[0][i], "moveSnake");
+}
+//设置一个变量，用来存储蛇移动的方向
+//假设 left right up down 分别代表四个方向
+var direction = "right";
+//添加键盘事件
+document.onkeydown = function(event){
+	event = event || window.event;
+	//为了合理性，蛇头和蛇尾不能交换方向
+	//当蛇向右移动时，不能点击向上按钮
+	if (direction == "right" && event.keyCode == 37) {
+		return;
+	} else if (direction == "down" && event.keyCode == 38) {
+		return;
+	} else if (direction == "left" && event.keyCode == 39){
+		return;
+	} else if (direction == "up" && event.keyCode == 40) {
+		return;
+	}
+	//点击不同按键，重新设置蛇移动的方向
+	switch (event.keyCode) {
+	case 37:
+		//alert("向左");left值减小
+		direction = "left";
+		break;
+	case 38:
+		//alert("向上");
+		direction = "up";
+		break;
+	case 39:
+		//alert("向右");
+		direction = "right";
+		break;
+	case 40:
+		//alert("向下");
+		direction = "down";
+		break;
+	}
+	console.log(direction);
+}
+
+//存储蛇头所在的位置坐标 --默认第一行，第三列单元格
+var x = 2;
+var y = 0;
+
+//处理蛇的移动
+var scoreNum = 0;//存储得分
+var timerMove = setInterval(function(){
+	//判断蛇移动的方向，从而设置移动的位置
+	switch (direction){
+		case "left":{
+			x--;
+			break;
+		}
+		case "right":{
+			x++;
+			break;
+		}
+		case "up":{
+			y--;
+			break;
+		}
+		case "down":{
+			y++;
+			break;
+		}
+		default:
+			break;
+	}
+	//为了合理性，判断碰到墙壁就GAMEOVER
+	if (x<0 || y<0 || x>=colNumber || y>= rowNumber) {
+		clearInterval(timerMove);
+		alert("gameover");
+		return;
+	}
+	//如果蛇即将移动的位置div，是蛇之前的身体所在的div，则游戏结束
+	for (var i = 0;i<snakeBodys.length;i++) {
+		//此时蛇身div和移动蛇头div对比，如果是同一个，则吃到身体
+		if (snakeBodys[i]== divSnake[y][x]) {
+			clearInterval(timerMove);
+			alert("gameover");
+			return;
+		}
+	}
+	//设置蛇身长度移动的过程
+	//如果蛇迟到食物，则蛇头和食物位置一致
+	if ( foodX == x && foodY == y) {	//吃到食物
+		//蛇头所在div的颜色
+		addClass(divSnake[y][x],"moveSnake");
+		removeClass(divSnake[y][x],"food");
+		//蛇头所在的div添加到蛇身数组中
+		snakeBodys.push(divSnake[y][x]);
+		randomFoods();
+		scoreNum++;
+		score.innerHTML = scoreNum;
+	} else{//没有吃到
+		//设置蛇尾的颜色
+		removeClass(snakeBodys[0],"moveSnake");
+		//删除头部第一个div
+		snakeBodys.shift();
+		//蛇头所在的div颜色
+		addClass(divSnake[y][x],"moveSnake");
+		//蛇头所在的div添加到蛇身数组中
+		snakeBodys.push(divSnake[y][x]);
+	}
+},200);
+
+touch.on(document,"touchstart",function(event){
+	event = event ||window.event;
+	event.preventDefault();
+})
+
+//右轻扫
+touch.on(document,"swiperight",function(event){
+	event = event ||window.event;
+	if (direction=="left") {
+		return;
+	}
+	direction = "right";
+})
+
+touch.on(document,"swipeleft",function(event){
+	event = event ||window.event;
+	if (direction=="right") {
+		return;
+	}
+	direction = "left";
+})
+
+touch.on(document,"swipeup",function(event){
+	event = event ||window.event;
+	if (direction=="down") {
+		return;
+	}
+	direction = "up";
+})
+touch.on(document,"swipedown",function(event){
+	event = event ||window.event;
+	if (direction=="up") {
+		return;
+	}
+	direction = "down";
+})
+
+//随机食物
+function randomFoods(){
+	foodX = randomNumber(0,colNumber-1);
+	foodY = randomNumber(0,rowNumber-1);
+	//设置食物的去重
+	if (hasClass(divSnake[foodY][foodX],"moveSnake")) {
+		randomFoods();	//重新随机食物位置
+	} else{
+		addClass(divSnake[foodY][foodX],"food");
+	}
+}
+randomFoods();
